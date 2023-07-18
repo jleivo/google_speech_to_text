7a4687cfff2d5b5cffa07ec5947c76b7282a2fd1
Juha Leivo <juha@leivo.org>
2023-07-18 21:03:52 +0200

    Added .env support
    Updated README.md & .gitignore to reflect changes
    Modified record_watcher to be more robust, check for env variables.
    Unified bit more the variable referencing

--- Readme.md
+++ Readme.md
@@ -1,14 +1,20 @@
-# Workflow: audio rec to task or note
+# Idea: audio rec to task or note
 
-This was to test if its feasible to get from a watch audio recording to a text or task, when language is finnish.
+This is working set of scripts to convert a m4a audio file to text using Google
+cloud text-to-speech service.
 
-## Flow
+## Workflow
 
-Watch recorder stores files to phone. Syncthing monitors the folder and copies files to server. Server has inotifywait waiting for files, it launches bash script which converts the m4a files to flac, sends the flacs to be translated by Google, then if on first word is "muistiinpano" stores it to Obsidian folder, otherwise its sent to Nozbe.
+User records audio via watch application. Watch application transfer recording
+as m4a file to phone. Syncthing monitors the folder and copies the file to a
+server. Server has inotifywait waiting for files, it launches bash script which converts the m4a files to flac, sends the flacs to be translated by Google, then
+if on first word is "muistiinpano" stores it to Obsidian folder, otherwise it's
+sent to Nozbe.
 
 ## Installation
 
-this is more of a technical list than step by step instructions, for example how to configure Syncthing is out of scope
+This is more of a technical list than step by step instructions, for example how
+to configure Syncthing is out of scope.
 
 ### os dependencies
 
@@ -29,6 +35,13 @@ pip install -r requirements.txt
 
 copy shared_functions.sh to /opt/scripts
 
+### Secrets
+
+scripts expects to receive following secrets:
+
+- GOOGLE_APPLICATION_CREDENTIALS
+- NOZBE_EMAIL
+
 ### Google cloud project
 
 TBD
@@ -36,18 +49,22 @@ TBD
 ## Usage
 
 1. configure Syncthing folders (audio recordings & Obsidian)
-2. set the script to run in tmux
+2. create .env file
+3. set the script to run in tmux
+
 ```bash
-/opt/scripts/record_watcher.sh -d /home/syncthing/audio/
+cd /opt/scripts/text-to-speech/;./record_watcher.sh -d /home/syncthing/audio/
 ```
 
 ## Limits
 
-We are using synchronious records to the max translate limit is ~1 min (ref https://cloud.google.com/speech-to-text/quotas)
+We are using synchronious records to the max translate limit is ~1 min
+(ref [Text-to-speech quotas](https://cloud.google.com/speech-to-text/quotas))
 
 Google python script requires X64 platform, doesn't work on ARM (Raspberrypi)
 
 ## Improvement ideas
+
 - [ ] Analyze file lenght and send it to storage if needed
 - [ ] Move to Nozbe for teams, due to (better) API.
 - [ ] Containerize the tool
--- record_watcher.sh
+++ record_watcher.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 #
 # Author: Juha Leivo
-# Version: 2
-# Date: 2023-05-05
+# Version: 3
+# Date: 2023-07-18
 #
 # History
 #   1 - 2023-04-29, initial write, monitors folder, converts items to flac
 #   2 - 2023-05-05, Minor typo fixed, improved task/note detection, corrected
 #       bug in detecting files with spaces in their names 
+#   3 - 2023-07-18, added support for .env file, made more robust
 
 readonly print_to_screen=1
 readonly obsidian_dir='/home/syncthing/Sync/Obsidian'
+readonly source_files='/opt/scripts/shared_functions.sh /opt/pythonvenv/google/bin/activate .env'
 
-source /opt/pythonvenv/google/bin/activate
-source /opt/scripts/shared_functions.sh
-export GOOGLE_APPLICATION_CREDENTIALS='/opt/pythonvenv/google/text-to-speech.json'
+source_fail=0
+for file in $source_files; do
+  if [[ -f "$file" ]]; then
+    source "$file"
+  else
+    echo "ERROR: "$file" not found"
+    source_fail=1
+  fi
+done
 
+if [[ "${source_fail}" == '1' ]]; then
+  exit 1
+fi
 ################################# FUNCTIONS ####################################
 debug=0
 
@@ -49,6 +60,10 @@ function print_usage() {
     echo ''
     echo 'Companion script to Googl text-to-speech, which monitors directory'
     echo 'for new files and converts them to flac, then sends them to translated' 
+    echo 'text-to-speech service.'
+    echo 'Application expects two environment variallbes to be set:'
+    echo '  GOOGLE_APPLICATION_CREDENTIALS and NOZBE_EMAIL'
+    echo 'You can use .env file in the scripts working directory if you so wish'
     echo ''
     echo 'Usage'
     echo ''
@@ -73,6 +88,7 @@ function log() {
 function init() {
 
   program_fail=0
+  env_param_fail=0
 
   # check if we have the programs
   for PROGRAM in inotifywait ffmpeg; do
@@ -86,32 +102,51 @@ function init() {
     return 1
   fi
 
+  # Check that secrets are set
+  if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
+    log "ERROR: GOOGLE_APPLICATION_CREDENTIALS variable not set" "${print_to_screen}"
+    env_param_fail=1
+  fi
+
+  if [[ -z "${NOZBE_EMAIL}" ]]; then
+    log "ERROR: NOZBE_EMAIL variable not set" "${print_to_screen}"
+    env_param_fail=1
+  fi
+
+  if [[ "${env_param_fail}" == '1' ]]; then
+    return 1
+  fi
 }
 
 function monitor_directory() {
 
-    echo "Monitoring directory: ${1}"
+    echo "Monitoring directory: $1"
     inotifywait -m "$1" -e create -e moved_to |
     while read dir action file; do
         # check if the contents of the variable file ends in m4a
         if [[ "${file##*.}" == "m4a" ]]; then
             flac_name=${file%.*}
-            echo "Converting file: $file"
-            ffmpeg -i "$1/$file" -vn -acodec flac "/tmp/$flac_name.flac" -loglevel fatal \
-            || { log "ERROR: ffmpeg failed" "${print_to_screen}"; send_mail "" "$file" 'leivo.0303@nozbe.com' "ffmpeg failed"; continue; }
-            echo "Converting file: $flac_name.flac to text via Google text-to-speech"
-            result=$(/opt/scripts/text_to_speech.py -f "/tmp/$flac_name.flac" || { log "ERROR: Text-to-speech failed" "${print_to_screen}"; })
+            echo "Converting file: ${file}"
+            ffmpeg -i "$1/$file" -vn -acodec flac "/tmp/${flac_name}.flac" \
+            -loglevel fatal \
+            || { log "ERROR: ffmpeg failed" "${print_to_screen}"; \
+            send_mail "" "${file}" "${NOZBE_EMAIL}" "ffmpeg failed"; continue; }
+            echo "Converting file: ${flac_name}.flac to \
+ text via Google text-to-speech"
+            result=$(/opt/scripts/text_to_speech.py -f "/tmp/${flac_name}.flac"\
+            || { log "ERROR: Text-to-speech failed" "${print_to_screen}"; })
             # if first word in the variable is "muistiinpano", then it's a note
-            # otherwise it's a tas
+            # otherwise it's a task
             # get the first word from the variable result
             task=$(echo $result|cut -d' ' -f2)
             if [[ $task == *muistiinpano* ]]; then
                 # echo everything after the first space in the variable result
                 echo "It's a note, adding to Obsidian"
-                echo $result | cut -d' ' -f2- > "$obsidian_dir/Inbox/$flac_name.md"
+                echo "${result}" | cut -d' ' -f2- \
+                                    > "$obsidian_dir/Inbox/${flac_name}.md"
                 mv "/tmp/$flac_name.flac" "$obsidian_dir/05 - media/"
                 echo "![[$flac_name.flac]]" >> "$obsidian_dir/Inbox/$flac_name.md"
-                rm "$1/$file"
+                rm "$1/${file}"
             else 
                 echo "It's a task or unknown thing, sending to Nozbe"
                 # the mail function uses also file parameter and messes it up somehow
@@ -119,15 +154,14 @@ function monitor_directory() {
                 # seems to have a problem with spaces in filenames, so we replace them
                 # with underscores.
                 nospacefile=${file// /_}
-                mv "$1/$file" "/tmp/$nospacefile"
-                send_mail "/tmp/$nospacefile" "empty" 'leivo.0303@nozbe.me' "$result"
-                rm "/tmp/$flac_name.flac"
-                rm "/tmp/$nospacefile"
+                mv "$1/${file}" "/tmp/${nospacefile}"
+                send_mail "/tmp/${nospacefile}" "empty" "${NOZBE_EMAIL}" "${result}"
+                rm "/tmp/${flac_name}.flac"
+                rm "/tmp/${nospacefile}"
             fi
         fi
     done
 }
-
 
 ##################################### LOGIC ####################################
 echo ''
